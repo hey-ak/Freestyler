cca5d7aae29620504e40339c002c4e8a42f59bbf
hey-ak <akshay0127.be21@chitkara.edu.in>
2025-06-17 21:22:47 +0530

    Initial Commit

---
+++ Freestyler/FreestylerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreestylerApp.swift
+//  Freestyler
+//
+//  Created by Akshay Jha on 17/06/25.
+//
+
+import SwiftUI
+
+@main
+struct FreestylerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
